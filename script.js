0cc12443d70a828a4f2684affbc718942f06edc7
Grandeman <joshuajmorales01@gmail.com>
2023-07-14 10:11:48 -0400

    Code now allows user to change the grid size via a prompt The number entered is then used in the loop to calc the size and the squares generated are a percentage of the size Added a clear button to clear the entire grid Made style changes

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-const container = document.querySelector('.inner-container');
-const grid = 256;
+const container = document.querySelector('.container');
+const innerContainer = document.querySelector('.inner-container');
 
-for (let i = 0; i < grid; i++) {    //Create loop for drawing squares
+function createGrid(gridSize = 16) {
+  innerContainer.innerHTML = '';
+
+  // Calculate square size as percentage
+  const squareSize = `${100 / gridSize}%`; 
+
+  // Loop to draw squares
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
     square.className = 'grid-square';
-    container.appendChild(square);
-
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = '#00FF41';
+      square.style.backgroundColor = '#00FF41';
     });
+    innerContainer.appendChild(square);
+  }
 
-};
+  const squares = document.querySelectorAll('.grid-square');
+  squares.forEach((square) => {
+    square.style.width = squareSize;
+    square.style.paddingBottom = squareSize;
+  });
+}
 
+createGrid();
+
+// Button to change grid size
+const gridSelector = document.querySelector('.grid-size');
+gridSelector.addEventListener('click', () => {
+  let choice = +prompt('Enter Grid Size (up to 100)', '');
+  if (choice <= 100 && choice > 1) {
+    createGrid(choice);
+  } else {
+    alert('Invalid grid size! Please enter a number between 2 and 100.');
+  }
+});
+
+// Button to clear grid
 const buttons = document.querySelector('.buttons');
-    const clear = document.createElement('button');
-    clear.className = 'clear-button';
-    clear.textContent = 'Clear';
-    clear.addEventListener('click', () => {
-        const squares = document.querySelectorAll('.grid-square');
-        squares.forEach(square => {
-        square.style.backgroundColor = '';
-        });
-    });
+const clear = document.createElement('button');
+clear.className = 'clear-button';
+clear.textContent = 'Clear';
+clear.addEventListener('click', () => {
+  const squares = document.querySelectorAll('.grid-square');
+  squares.forEach((square) => {
+    square.style.backgroundColor = '';
+  });
+});
 
-    buttons.appendChild(clear);
-
-
+buttons.appendChild(clear);
