d6d22f010b218605f727f08aef80617ef1f90c66
Grandeman <joshuajmorales01@gmail.com>
2023-07-14 05:34:27 -0400

    Add mouseover function and clear button

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ for (let i = 0; i < grid; i++) {    //Create loop for drawing squares
     const square = document.createElement('div');
     square.className = 'grid-square';
     container.appendChild(square);
+
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    });
+
 };
 
+const buttons = document.querySelector('.buttons');
+    const clear = document.createElement('button');
+    clear.className = 'clear-button';
+    clear.textContent = 'Clear';
+    clear.addEventListener('click', () => {
+        const squares = document.querySelectorAll('.grid-square');
+        squares.forEach(square => {
+        square.style.backgroundColor = '';
+        });
+    });
 
+    buttons.appendChild(clear);
 
 
