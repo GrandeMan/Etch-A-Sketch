b907e675f80a5bfb11be3cbfcf4d2a35ea8978f1
Grandeman <joshuajmorales01@gmail.com>
2023-07-15 10:10:09 -0400

    Change hover effect to click and drag effect

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 const container = document.querySelector('.container');
 const innerContainer = document.querySelector('.inner-container');
 
+// Function to draw when mouse is down and stop drawing when up
+
+let isDrawing = false;
+
+function whenMouseDown(down) {
+    isDrawing = true;
+    down.target.style.backgroundColor = '#00FF41';
+}
+
+function whenMouseEnter(enter) { //mouseover does not work because it bubbles up DOM tree, whereas mouseenter only affects the target element
+    if (isDrawing) {
+        enter.target.style.backgroundColor = '#00FF41';
+    }
+}
+
+function whenMouseUp() {
+    isDrawing = false;
+}
+
+
 function createGrid(gridSize = 16) {
-  innerContainer.innerHTML = '';
+    innerContainer.innerHTML = '';
 
-  // Calculate square size as percentage
-  const squareSize = `${100 / gridSize}%`; 
+    // Calculate square size as percentage
+    const squareSize = `${100 / gridSize}%`; 
+    
 
-  // Loop to draw squares
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement('div');
-    square.className = 'grid-square';
-    square.addEventListener('mouseover', () => {
-      square.style.backgroundColor = '#00FF41';
+    // Loop to draw squares
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.className = 'grid-square';
+        square.addEventListener('mousedown', whenMouseDown);
+        square.addEventListener('mouseenter', whenMouseEnter);
+        innerContainer.appendChild(square);
+    }
+
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach((square) => {
+        square.style.width = squareSize;
+        square.style.paddingBottom = squareSize;
     });
-    innerContainer.appendChild(square);
-  }
 
-  const squares = document.querySelectorAll('.grid-square');
-  squares.forEach((square) => {
-    square.style.width = squareSize;
-    square.style.paddingBottom = squareSize;
-  });
+    window.addEventListener('mouseup', whenMouseUp);
+
 }
 
 createGrid();
@@ -29,12 +52,12 @@ createGrid();
 // Button to change grid size
 const gridSelector = document.querySelector('.grid-size');
 gridSelector.addEventListener('click', () => {
-  let choice = +prompt('Enter Grid Size (up to 100)', '');
-  if (choice <= 100 && choice > 1) {
-    createGrid(choice);
-  } else {
-    alert('Invalid grid size! Please enter a number between 2 and 100.');
-  }
+    let choice = +prompt('Enter Grid Size (up to 100)', '');
+    if (choice <= 100 && choice > 1) {
+        createGrid(choice);
+    } else {
+        alert('Invalid grid size! Please enter a number between 2 and 100.');
+    }
 });
 
 // Button to clear grid
@@ -43,10 +66,10 @@ const clear = document.createElement('button');
 clear.className = 'clear-button';
 clear.textContent = 'Clear';
 clear.addEventListener('click', () => {
-  const squares = document.querySelectorAll('.grid-square');
-  squares.forEach((square) => {
-    square.style.backgroundColor = '';
-  });
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach((square) => {
+        square.style.backgroundColor = '';
+    });
 });
 
 buttons.appendChild(clear);
